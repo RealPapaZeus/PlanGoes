02fe676358443e3686cc304ebcbe2e9caa2a8849
Adrian Sarwari <sarwariadrian@gmail.com>
2019-10-16 21:03:40 +0200

    initialised UserName class

---
+++ lib/UserName.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class UserName extends StatefulWidget {
+
+  UserName({Key key,}) : super(key: key);
+
+  @override
+  _UserNameState createState() => new _UserNameState();
+}
+  
+class _UserNameState extends State<UserName>{
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text('Initialise UserName'),
+      ),
+      body: new Center(
+        child: new FlatButton(
+          onPressed: (){},
+          textColor: Theme.of(context).accentColor,
+          child: new Text('Apply'),
+        )
+      ),
+    );
+  }
+}
