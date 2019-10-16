ba0008713cb9b9015c5707d2e04b97df6389da3c
Adrian Sarwari <sarwariadrian@gmail.com>
2019-10-16 20:23:20 +0200

    LogIn class

---
+++ lib/LogIn.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class LogIn extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyLogInPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyLogInPage extends StatefulWidget {
+  MyLogInPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyLogInPageState createState() => _MyLogInPageState();
+}
+
+class _MyLogInPageState extends State<MyLogInPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('LogIn'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      )
+    );
+  }
+}
