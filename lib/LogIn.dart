87ee600c1316e8ec5660ad4989568fc95b9a9e5a
Adrian Sarwari <sarwariadrian@gmail.com>
2019-10-20 18:07:26 +0200

    LogIn user can sign in with email and password

--- lib/LogIn.dart
+++ lib/LogIn.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:plan_go_software_project/CreateAccount.dart';
 import 'package:plan_go_software_project/EventList.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LogIn extends StatelessWidget {
 
@@ -26,86 +27,81 @@ class MyLogInPage extends StatefulWidget {
 
 class _MyLogInPageState extends State<MyLogInPage> {
 
-  //variables for LogIn Page
   String _email;
   String _password;
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override 
+  void dispose(){
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void signIn() async{
+    final _formState = _formKey.currentState;
+
+    if(_formState.validate()){
+      _formState.save();
+
+      try{
+        AuthResult user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _emailController.text, password: _passwordController.text);
+        Navigator.push(context, MaterialPageRoute(builder: (context) => EventList())); 
+        print(_email);
+      }catch(e){
+        print(e.message);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         centerTitle: true,
-        title: Text('LogIn'),
+        title: Text('Events'),
       ),
       body: new Form(
         key: _formKey,
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              TextFormField(
-                validator: (input) {
-                  if(input.isEmpty){
-                    return 'Please enter an Email';
-                  }
-                },
-                onSaved: (input) => _email == input,
-                decoration: InputDecoration(
-                  labelText: 'Email'
-                ),
+        child: new Column(
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email'
               ),
-              TextFormField(
-                validator: (input) {
-                  if(input.length < 6){
-                    return 'Password is too short';
-                  }
-                },
-                onSaved: (input) => _password == input,
-                decoration: InputDecoration(
-                  labelText: 'Password'
-                ),
-                obscureText: true,
+              validator: (value) => value.isEmpty ? 'Please enter Email' : null,
+              onSaved: (value) => _email == value,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Enter Password'
               ),
-              FloatingActionButton(
-                onPressed: (){},
-                child: Text('Sign in')
-              ),
-              FlatButton(
-                onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CreateAccount()));
-                },
-                textColor: Theme.of(context).accentColor,
-                child: new Text('Create Account?'),
-              )
-            ],
-          )
+              obscureText: true,
+              validator: (value) => value.isEmpty ? 'Please enter Password' : null,
+              onSaved: (value) => _password == value,
+            ),
+            RaisedButton(
+              onPressed: signIn,
+              child: Text('Sign in')
+            ),
+            FlatButton(
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CreateAccount()));
+              },
+              textColor: Theme.of(context).accentColor,
+              child: new Text('Create Account?'),
+            ), 
+          ],
         )
       ), 
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(context,
-          MaterialPageRoute(builder: (context) => EventList()));
-        },
-        child: Icon(Icons.accessible),
-      )
     );
   }
-
-  void _signIn(){
-    final _formState = _formKey.currentState;
-
-    if(_formState.validate()){
-      
-    }
-  }
-
-  // Widget buildForm(){
-  //   return Form(
-  //     child: 
-  //   )
-
-  // }
 }
