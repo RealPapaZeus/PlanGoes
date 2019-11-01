4351e7826456575e9bba1d55141b6aa1574b02b7
Adrian Sarwari <sarwariadrian@gmail.com>
2019-11-01 12:45:48 +0100

    event gets added into firebase

--- lib/EventView/RegisterEvent.dart
+++ lib/EventView/RegisterEvent.dart
@@ -1,4 +1,9 @@
+import 'dart:isolate';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RegisterEvent extends StatefulWidget {
 
@@ -12,10 +17,53 @@ class _RegisterEventState extends State<RegisterEvent> {
 
   String _eventName;
   String _description;
+  bool _isLoading = false;
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _eventNameController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+ 
+  Future<void> createEvent(String eventName, String description, String userID) async {
+    final databaseReference = Firestore.instance;
+
+    await databaseReference.collection("events").
+      document().
+      setData({
+        'eventName': '$eventName',
+        'description': '$description',
+        'admins' : ["$userID"] 
+      });
+  }
+  
+  void registerEventByPress() async {
+    final _formState = _formKey.currentState;
+    final FirebaseUser user = await FirebaseAuth.instance.currentUser();
+
+    setState(() {
+      _isLoading = true;  
+    });
+
+    if(_formState.validate()) {
+      _formState.save();
+
+      try{
+        createEvent(_eventNameController.text.toString(),
+                    _descriptionController.text.toString(), user.uid.toString());
+
+        setState(() {
+          _isLoading = false;  
+        });
+
+      } catch(e) {
+        
+        setState(() {
+          _isLoading = false;  
+        });
+
+        print(e.message);
+      }
+    }
+  }
 
   Widget eventNameTextField() {
     return TextFormField(
@@ -43,6 +91,20 @@ class _RegisterEventState extends State<RegisterEvent> {
       validator: (value) => value.isEmpty ? 'Please enter some description' : null,
       onSaved: (value) => _description == value,
     );
+  }
+
+  List<Widget> userViewConfirmNewAccount() {
+    return [
+      Padding(
+        padding: EdgeInsets.all(15.0),
+        child: _isLoading
+                  ? Center(child: new CircularProgressIndicator())
+                  : RaisedButton(
+                      onPressed: registerEventByPress,
+                      child: Text('Register Event'),
+                  )
+        )
+    ];
   }
 
   List<Widget> inputWidgets() {
@@ -75,7 +137,8 @@ class _RegisterEventState extends State<RegisterEvent> {
                         key: _formKey,
                         child: new Column(
                           children: 
-                            inputWidgets()
+                            inputWidgets() + userViewConfirmNewAccount()
+                            
                         )
                       )
                     )
