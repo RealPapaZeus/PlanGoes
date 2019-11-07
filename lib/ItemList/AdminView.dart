a6a675d6f78b4d930b5a831b07620000b7b5d9d6
Adrian Sarwari <sarwariadrian@gmail.com>
2019-11-07 22:40:28 +0100

    admin view class declared

---
+++ lib/ItemList/AdminView.dart
@@ -0,0 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:plan_go_software_project/ItemList/ItemAlertDialog.dart';
+
+class AdminView extends StatefulWidget {
+
+  final String documentID;
+
+  AdminView({
+    Key key,
+    this.documentID
+    }) : super(key: key);
+
+  @override
+  _AdminViewState createState() => new _AdminViewState();
+}
+  
+class _AdminViewState extends State<AdminView>{
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  Widget createAppBar() {
+    return new AppBar(
+      centerTitle: true,
+      title: Text("${widget.documentID}"),
+    );
+  }
+
+  Widget createItem() {
+    return new FloatingActionButton(
+      elevation: 4.0,
+      child: const Icon(Icons.create),
+      onPressed: () {
+        showDialog(
+          context: context,
+          child: new ItemAlertView()
+        );
+      },
+    );
+  }
+
+  Widget bottomNavigation() {
+    return new BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      notchMargin: 4.0,
+      color: Colors.blue,
+      child: new Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.insert_invitation),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: createAppBar(),
+      body: new Center(
+        child: new FlatButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          textColor: Theme.of(context).accentColor,
+          child: new Text('Back to EventList'),
+        )
+      ),
+      floatingActionButton: createItem(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: bottomNavigation(),
+    );
+  }
+}
