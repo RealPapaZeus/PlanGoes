9fbb74e6ca5171bfe5271224c8f2d04945eef7a6
Fabian-Malte Möller <fabian-maltemoller@FabianMaltesMBP.fritz.box>
2019-11-09 13:17:57 +0100

    pic added

--- lib/EventView/EventList.dart
+++ lib/EventView/EventList.dart
@@ -171,12 +171,26 @@ class _EventListState extends State<EventList>{
                  ),
                ], 
              ),
-           )
-                ]
-            )
+           ),
+          new Container(
+           margin: new EdgeInsets.symmetric(
+           vertical: 16.0
+           ),
+           alignment: FractionalOffset.centerLeft,
+           child: new Image(
+            image: new AssetImage("assets/img/mars.png"),
+            height: 92.0,
+            width: 92.0,
+           ),
+         )
+        ]
+            
+   ),
+ );
+            
       
-    );
-    
+  
+
      
 
   }
