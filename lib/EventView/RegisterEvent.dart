0bb7c6baeaae81619de800d39fec7189bcfdc17e
donfour10 <dustin@werner-sippe.de>
2019-11-10 20:57:25 +0100

    added new packages

--- lib/EventView/RegisterEvent.dart
+++ lib/EventView/RegisterEvent.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:plan_go_software_project/EventView/EventList.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 class RegisterEvent extends StatefulWidget {
 
