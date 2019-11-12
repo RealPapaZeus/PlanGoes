57197fc9593fe7ae1c4980d1367514b2f7659da2
donfour10 <dustin@werner-sippe.de>
2019-11-12 15:58:32 +0100

    added material color picker package

--- lib/EventView/RegisterEvent.dart
+++ lib/EventView/RegisterEvent.dart
@@ -9,7 +9,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as p;
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
+import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
 
 class RegisterEvent extends StatefulWidget {
 
