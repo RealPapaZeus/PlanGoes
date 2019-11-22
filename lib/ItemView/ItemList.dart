3f0d6e2b034d276d42d23f0ac2b371d8080e57e9
Adrian Sarwari <sarwariadrian@gmail.com>
2019-11-22 23:29:37 +0100

    user can swipe items

--- lib/ItemView/ItemList.dart
+++ lib/ItemView/ItemList.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:plan_go_software_project/ItemView/ItemPickDialog.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ItemList extends StatefulWidget {
 
@@ -49,8 +50,9 @@ class _ItemListState extends State<ItemList>{
             itemExtent: 100,
             padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 50.0),
             itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index) =>
-              buildItemList(context, snapshot.data.documents[index]),
+            itemBuilder: (context, index) {
+              return buildItemList(context, snapshot.data.documents[index]);
+            }
         );
       },
     );
@@ -58,56 +60,68 @@ class _ItemListState extends State<ItemList>{
 
   Widget buildItemList(BuildContext context, DocumentSnapshot document) {
     return new GestureDetector(
-      onTap: (){
-        showDialog(
-          context: context,
-          child: new ItemPickDialog(userId: widget.userId,
-                                    documentId: widget.documentId,
-                                    itemDocumentId: document.documentID.toString())
-        );
-      },
-      child: Container(
-        child: Card(
-        elevation: 10.0,
-        margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Row(
+        onTap: (){
+          showDialog(
+            context: context,
+            child: new ItemPickDialog(userId: widget.userId,
+                                      documentId: widget.documentId,
+                                      itemDocumentId: document.documentID.toString())
+          );
+        },
+        child: Container(
+          child: Card(
+          elevation: 10.0,
+          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+            child: Slidable(
+                actionPane: SlidableDrawerActionPane(),
+                //actionExtentRatio: 0.25,
+                closeOnScroll: false,
+                secondaryActions: <Widget>[
+                  IconSlideAction(
+                    caption: 'Delete',
+                    color: Colors.red,
+                    icon: Icons.delete,
+                    onTap: (){},
+                  )
+                ],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(right: 12.0, left: 12.0),
-                  decoration: new BoxDecoration(
-                    border: new Border(
-                      right: new BorderSide(width: 1.0, color: Colors.black26)
-                    )
-                  ),
-                  child: Icon(Icons.person),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.only(right: 12.0, left: 12.0),
+                      decoration: new BoxDecoration(
+                        border: new Border(
+                          right: new BorderSide(width: 1.0, color: Colors.black26)
+                        )
+                      ),
+                      child: Icon(Icons.person),
+                    ),
+                    Container(
+                      constraints: BoxConstraints(maxWidth: 250),
+                      padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                      child: Text(
+                        document['name'],
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ), 
+                  ],
                 ),
                 Container(
-                  constraints: BoxConstraints(maxWidth: 250),
-                  padding: const EdgeInsets.only(left: 12.0, right: 12.0),
-                  child: Text(
-                    document['name'],
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ), 
-              ],
+                  padding: const EdgeInsets.only(right: 12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Text('0'),
+                      Text('/'),
+                      Text(document['value'].toString())
+                    ],)
+                )          
+              ]
             ),
-            Container(
-              padding: const EdgeInsets.only(right: 12.0),
-              child: Row(
-                children: <Widget>[
-                  Text('0'),
-                  Text('/'),
-                  Text(document['value'].toString())
-                ],)
-            )          
-          ]
         ),
       ),
-    ),
-   );
+    ));
   }
 
   @override
