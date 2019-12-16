aabe29f83a6c08c0492cc380eb885b68a1a5dab5
Adrian Sarwari <sarwariadrian@gmail.com>
2019-12-16 16:14:27 +0100

    dynamic link class added

---
+++ lib/DynamicLink/DynamicLink.dart
@@ -0,0 +1,26 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+
+// class DynamicLink extends StatefulWidget {
+
+//   DynamicLink({
+//     Key key,
+//     }) : super(key: key);
+
+//   @override
+//   _DynamicLinkState createState() => new _DynamicLinkState();
+// }
+
+// class _DynamicLinkState extends State<DynamicLink> {
+  
+//   final DynamicLinkParameters parameters = DynamicLinkParameters(
+//     link: Uri.parse('https://plangosoftwareproject.page.link/'),
+//   );
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return null;
+//   }
+// }
