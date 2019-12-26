e8f52fc6e51095dc86c1bbbefe5c5a5bac16edd4
Adrian Sarwari <sarwariadrian@gmail.com>
2019-12-26 15:37:20 +0100

    added new color

--- lib/colors.dart
+++ lib/colors.dart
@@ -6,6 +6,8 @@ const cPlanGoBlue = const Color(0xFF1e48b4);
 const cPlanGoMarineBlueDark = const Color(0xFF00007b);
 const cPlanGoBlueGrey = const Color(0xFF83a6c6);
 
+const cPlanGoDark = const Color(0xFF00002a);
+
 const cPlanGoWhiteBlue = const Color(0xFFf4f9ff);
 
 const cPlanGoRedBright = const Color(0xFFff0000);
